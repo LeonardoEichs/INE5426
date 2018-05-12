0adae9ba33fa9c9aadeab8272189f2f58aba083d
jr- <joseluis.ruas@gmail.com>
2018-05-12 15:53:38 -0300

    exitDeclaration and anotations

--- SyntaticAnalyzer/antlr/AMZSemanticListener.java
+++ SyntaticAnalyzer/antlr/AMZSemanticListener.java
@@ -6,6 +6,11 @@ public class AMZSemanticListener extends AMZ_syntBaseListener {
 
 	public SymbolTable symbolTable;
 
+	//anotacoes
+	private ParseTreeProperty<String> types = new ParseTreeProperty<>();
+	private ParseTreeProperty<String> id = new ParseTreeProperty<>();
+	private ParseTreeProperty<Integer> sizes = new ParseTreeProperty<>();
+
 	public void enterEval(AMZ_syntParser.EvalContext ctx) {
 		symbolTable = new SymbolTable(null); // Initialize symbol table
 		System.out.println("Symbol table created");
@@ -31,6 +36,33 @@ public class AMZSemanticListener extends AMZ_syntBaseListener {
 
 		}
 	}
+
+	//declaration : type ID array_position? ;
+	//symbol.type VARIABLE, FUNCTION, OBJECT
+	//simple_command :   declaration EQUALS expression | declaration;
+	//function_block : declaration LPAREN parameters RPAREN command_block ;
+	//parameters : (declaration (COMMA declaration)*)? ;
+	// object_element : declaration COLON expression ;
+	//verifica se ID ja foi declarado se não anota
+	public void exitDeclaration(AMZ_syntParser.DeclarationContext ctx) {
+		String id = ctx.ID().getText();
+
+		Symbol symbol = symbolTable.lookup(id);
+		//verifica se o simbolo(ID) ja ta na tabela de simbolos se tiver printa erro semantico e retorna
+		if (symbol != null) {
+			switch (symbol.type) {
+				case VARIABLE:
+					System.err.println("Variável já declarada");
+					break;
+				case FUNCTION:
+					System.err.println("Função já declarada");
+					break;
+				case OBJECT:
+					System.err.println("Objeto já declarada");
+					break;
+				}
+				return;
+		}
 
 
 
